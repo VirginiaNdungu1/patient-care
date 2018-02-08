fbd663da73f8168e1b452f1b842e6a590f79c3c8
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2018-02-08 16:25:01 +0300

    * import Output and EventEmitter functions .
    * create New patient (newPatient) property
    * create new EventEmitter object (addPatient) of type Patient.
    
    * Call the emit method and pass in the newPatient property.

--- src/app/patient-form/patient-form.component.ts
+++ src/app/patient-form/patient-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+// import Output, EventEmitter functions
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
 // import Patient Class
 import { Patient } from '../patient'
 
@@ -10,6 +12,14 @@ import { Patient } from '../patient'
 export class PatientFormComponent implements OnInit {
   // create a new patient property
   newPatient = new Patient(0, '', '', '', '', '');
+
+  // create new EventEmitter object (addPatient) of type Patient
+  @Output() addPatient = new EventEmitter<Patient>();
+
+  // Call the emit method and pass in the newPatient property
+  submitPatient() {
+    this.addPatient.emit(this.newPatient)
+  }
   constructor() { }
 
   ngOnInit() {
