4809eceac1bbff97883f6eb578d05de77bff02f5
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2018-02-08 14:09:50 +0300

    register FormsModule
    Declare new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { PatientComponent } from './patient/patient.component';
+import { PatientFormComponent } from './patient-form/patient-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PatientComponent
+    PatientComponent,
+    PatientFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
