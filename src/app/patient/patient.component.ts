24f9886527f0546c2cd491fb241802652e1453d1
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2018-02-08 18:14:46 +0300

    Register Service
    * import PatientService class
    * Add to array of providers inside @Component
    
    Update patient Property - type Patient
    
    * inject service tot he constructor
    
    * Call the getPatients()
     method
    
    * returns the array of patients

--- src/app/patient/patient.component.ts
+++ src/app/patient/patient.component.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Patient } from '../patient-model';
 import { Patients } from '../patients';
+import { PatientService } from '../patients/patient.service';
 
 @Component({
   selector: 'app-patient',
   templateUrl: './patient.component.html',
+  providers: [PatientService],
   styleUrls: ['./patient.component.css']
 })
 export class PatientComponent implements OnInit {
-  // patients = [
-  //   new Patient(1, 'Nish', 'Bernards', '1994/8/8', '+254 701 100 175', 'ndungu.wairimu22@gmail.com'),
-  //   new Patient(2, 'Joy', 'Lorna', '1998/8/8', '+254 701 100 560', 'joy.lorna@gmail.com'),
-  //   new Patient(3, 'Sam', 'Maundu', '1994/9/10', '+254 703 973 073', 'sam.maundu@gmail.com'),
-  //   new Patient(4, 'Purity', 'Wanjiku', '1984/5/31', '+254 712 234 567', 'wanjiku.purity@gmail.com'),
-  //   new Patient(5, 'Kevin', 'Mbuthia', '1990/11/15', '+254 715 101 318', 'kevin.mbuthia@gmail.com'),
-  //   new Patient(6, 'Newton', 'Kiragu', '1997/7/10', '+254 712 824 175', 'kiragu.newton@gmail.com'),
-  // ]
-  patients = Patients;
+  // update property patients - give it a type Patient
+  patients: Patient[];
+  // inject the service to the constructor
+  // Call the getPatients() method - returns an array of patients
+  // assign it to the patients property
+  constructor(patientService: PatientService) {
+    this.patients = patientService.getPatients()
+  }
+
+  ngOnInit() {
+  }
 
   // create the addNewPatient function - takes patient object as an arg
   // find length of the array of patients
@@ -29,9 +33,6 @@ export class PatientComponent implements OnInit {
 
   }
 
-  constructor() { }
 
-  ngOnInit() {
-  }
 
 }
