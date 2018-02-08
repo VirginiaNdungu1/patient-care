42ceae7f659c99423040cfb522f70ee68e5104f3
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2018-02-08 17:48:16 +0300

    * create separate file to store our patients array

---
+++ src/app/patients.ts
@@ -0,0 +1,10 @@
+import { Patient } from './patient-model';
+
+export var Patients: Patient[] = [
+  new Patient(1, 'Nish', 'Bernards', '1994/8/8', '+254 701 100 175', 'ndungu.wairimu22@gmail.com'),
+  new Patient(2, 'Joy', 'Lorna', '1998/8/8', '+254 701 100 560', 'joy.lorna@gmail.com'),
+  new Patient(3, 'Sam', 'Maundu', '1994/9/10', '+254 703 973 073', 'sam.maundu@gmail.com'),
+  new Patient(4, 'Purity', 'Wanjiku', '1984/5/31', '+254 712 234 567', 'wanjiku.purity@gmail.com'),
+  new Patient(5, 'Kevin', 'Mbuthia', '1990/11/15', '+254 715 101 318', 'kevin.mbuthia@gmail.com'),
+  new Patient(6, 'Newton', 'Kiragu', '1997/7/10', '+254 712 824 175', 'kiragu.newton@gmail.com'),
+]
