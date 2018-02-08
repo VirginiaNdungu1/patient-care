e6a3a574b539da9c8bad43915f2add44d8d9dcd0
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2018-02-08 14:08:55 +0300

    Minor Changes - proper ids for new patient instance

--- src/app/patient/patient.component.ts
+++ src/app/patient/patient.component.ts
@@ -8,11 +8,11 @@ import { Patient } from '../patient';
 export class PatientComponent implements OnInit {
   patients = [
     new Patient(1, 'Nish', 'Bernards', '1994/8/8', '+254 701 100 175', 'ndungu.wairimu22@gmail.com'),
-    new Patient(1, 'Joy', 'Lorna', '1998/8/8', '+254 701 100 560', 'joy.lorna@gmail.com'),
-    new Patient(1, 'Sam', 'Maundu', '1994/9/10', '+254 703 973 073', 'sam.maundu@gmail.com'),
-    new Patient(1, 'Purity', 'Wanjiku', '1984/5/31', '+254 712 234 567', 'wanjiku.purity@gmail.com'),
-    new Patient(1, 'Kevin', 'Mbuthia', '1990/11/15', '+254 715 101 318', 'kevin.mbuthia@gmail.com'),
-    new Patient(1, 'Newton', 'Kiragu', '1997/7/10', '+254 712 824 175', 'kiragu.newton@gmail.com'),
+    new Patient(2, 'Joy', 'Lorna', '1998/8/8', '+254 701 100 560', 'joy.lorna@gmail.com'),
+    new Patient(3, 'Sam', 'Maundu', '1994/9/10', '+254 703 973 073', 'sam.maundu@gmail.com'),
+    new Patient(4, 'Purity', 'Wanjiku', '1984/5/31', '+254 712 234 567', 'wanjiku.purity@gmail.com'),
+    new Patient(5, 'Kevin', 'Mbuthia', '1990/11/15', '+254 715 101 318', 'kevin.mbuthia@gmail.com'),
+    new Patient(6, 'Newton', 'Kiragu', '1997/7/10', '+254 712 824 175', 'kiragu.newton@gmail.com'),
   ]
   constructor() { }
 
