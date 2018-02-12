9e7f75ae9081f6c79b6de0cebef70f92c5930239
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2018-02-12 10:46:12 +0300

    import the custom pipe - FilterPipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { FilterPipe } from './pipes';
+
 
 
 import { AppComponent } from './app.component';
@@ -21,6 +23,7 @@ import { EditPatientComponent } from './edit-patient/edit-patient.component';
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    FilterPipe
   ],
   providers: [],
   bootstrap: [AppComponent]
