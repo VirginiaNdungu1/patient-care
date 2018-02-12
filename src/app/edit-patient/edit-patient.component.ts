7cedc8b0818525b00fac99e3f9601e2a266a9ebb
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2018-02-12 09:58:25 +0300

    importations

--- src/app/edit-patient/edit-patient.component.ts
+++ src/app/edit-patient/edit-patient.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-edit-patient',
